ecd0c32e4d6d42b67a36a67c6417d1e6343e65b3
Kolotushkin Danil <socurahlem@yandex.ru>
2022-12-11 17:54:06 +0300

    Removed test_data from git index

--- test_data/include/some/library.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LIBRARY_HPP
-#define LIBRARY_HPP
-
-#include <string_view>
-
-void printString(std::string_view sv);
-
-#endif //LIBRARY_HPP
--- test_data/source/library.cpp
+++
@@ -1,8 +0,0 @@
-#include <some/library.hpp>
-
-#include <iostream>
-
-void printString(std::string_view sv)
-{
-    std::cout << sv << std::endl;
-}
