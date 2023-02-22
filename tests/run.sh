4508ccc54ea7ad85fa242e7c00ad51547f2ded3c
vrodedanya <socurahlem@yandex.ru>
2023-02-23 01:17:48 +0300

    Some improves in run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/bash
 
-ABS_PATH=../target/debug/abs
+ABS_PATH:=../target/debug/abs
+PROFILE:=debug
+
+IS_FAILED=false
 
 for d in */ ; do
     echo "$d"
     cd $d
-    ../$ABS_PATH build -p release
+    ../$ABS_PATH build -p $PROFILE
     if [[ "$?" != "0" ]]; then
-        echo "Failed $d"
-        exit 1
+        echo "========Failed $d========"
+        IS_FAILED=true
+    else
+        echo "========Done $d========"
     fi
-    echo $?
     cd ..
 done
+
+if $IS_FAILED ; then
+    exit 1
+fi
